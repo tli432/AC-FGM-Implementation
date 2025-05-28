50379ca112c6cb46b8b5003f9aa83f7257378dfb
Tianjiao Li <tli432@gatech.edu>
2025-05-27 21:34:58 -0700

    Added FISTA and adaptive restart

--- first_order_methods.py
+++ first_order_methods.py
@@ -106,7 +106,7 @@ class First_Order_Methods:
         
         return np.array(values), x_bar, np.array(num_oracles), T_2 - T_1
 
-    def universal_AC_FGM(self, k, L_0=None, gamma=None, beta=0.184, alpha=0.1, x_0=None, eps=0, initial_line_search=True):
+    def universal_AC_FGM(self, k, L_0=None, gamma=None, beta=0.184, alpha=0.1, x_0=None, eps=0, initial_line_search=True, adaptive_restart=False):
         """
         AC-FGM method (universal version)
 
@@ -119,6 +119,7 @@ class First_Order_Methods:
         x_0: the initialized search point
         eps: pre-set accuracy for universal objectives. If the function is smooth, set eps = 0.
         initial line search: binary variable, decide whether to do an initial line search step.
+        adaptive restart: binary variable, decide whether to implement the adaptive restart rule.
 
         Output: function values in all iterations, output solution, cumulative number of oraclue calls, total run time
         """
@@ -158,6 +159,7 @@ class First_Order_Methods:
                 tmp2 = L_new * (np.linalg.norm(x_new-x))**2 / 2 + eps/4
                 if tmp1 <= tmp2:
                     print("Number of calls used for the initial linesearch: %d"%(i+1))
+                    initial_eta = eta
                     break
             
             if i == 199:
@@ -168,6 +170,7 @@ class First_Order_Methods:
             # If no line search: determine eta_1 based on L_0
             L_new = L_0
             eta = 1 / 2.5 /L_new
+            initial_eta = eta
             x_new = self.prob.prox_mapping(x, eta*g, eta*gamma)
             f_new, obj_new, g_new = self.prob.first_order_oracle(x_new)
             oracles += 1
@@ -204,6 +207,13 @@ class First_Order_Methods:
                 L_e = np.linalg.norm(g_new - g)**2/(2*(f-f_new-np.dot(g_new, x-x_new))+ eps/tau)
                 if L_e < 0:
                     L_e = 0
+            if adaptive_restart == True:
+                # Adaptive restart rule
+                if obj_new > obj:
+                    tau_old = 0
+                    tau = 1
+                    eta = initial_eta
+                    y = x_new                
             obj = obj_new
             f = f_new
             g = g_new
@@ -345,7 +355,7 @@ class First_Order_Methods:
                 oracles += 1
                 tmp1 = f_new - f - np.dot(g, x_new-x)
                 tmp2 = L_new/2 * (np.linalg.norm(x_new-x))**2 + eps/2
-                if tmp1 <= tmp2:
+                if tmp1 <= tmp2 + 1e-15 * f_new:
                     break
             f = f_new
             x = x_new
@@ -414,7 +424,7 @@ class First_Order_Methods:
                 oracles += 2
                 tmp1 = f_y - f_x - np.dot(g_new, y_new - x_new)
                 tmp2 = L_new/2 * (np.linalg.norm(y_new-x_new))**2 + eps * tau/2
-                if tmp1 <= tmp2:
+                if tmp1 <= tmp2  + 1e-15 * f_y:
                     break
             x = x_new
             y = y_new
@@ -432,6 +442,80 @@ class First_Order_Methods:
         
         return np.array(values), y, np.array(num_oracles), T_2 - T_1
     
+
+    def FISTA(self, k, L_0=None, gamma=None, x_0=None, beta=2, adaptive_restart=False):
+        """
+        The FISTA method with backtracking in Beck and Teboulle (2009)
+
+        Input: 
+        k: number of iterations
+        L_0: initial guess of the Lipschitz smooth constant. If L_0 = None, the method will use a local L_0 around x_0
+        gamma: scale of the regularizer, if applicable, usually set as prob.gamma
+        x_0: the initialized search point
+        beta: line search parameter. For the sake of consistency with UniversalFGM, we take the default value of beta as 2
+        adaptive restart: binary variable, decide whether to implement the adaptive restart rule.
+
+        Output: function values in all iterations, output solution, cumulative number of oraclue calls, total run time
+        """
+        print("FISTA: k=%d"%(k))
+        T_1 = time.clock()
+        if gamma is None:
+            gamma = self.prob.gamma
+        if not x_0:
+            x_0 = self.x_0.copy()
+        x = x_0.copy()
+        f, obj, g = self.prob.first_order_oracle(x)
+        values = [obj]
+        oracles = 1
+        num_oracles = [1]
+        y = x.copy()
+        t_k = 1
+        if L_0 is None:
+            x_eps = x - np.ones(self.prob.d) * 0.1
+            g_eps = self.prob.gradient(x_eps)
+            L_0 = np.linalg.norm(g_eps-g)/np.linalg.norm(x_eps-x)/4
+            oracles += 1
+        L = L_0
+
+        for t in range(1, k+1):
+            f_y, _, g_new = self.prob.first_order_oracle(y)
+            oracles += 1
+            for i in range(1000):
+                L_new = beta**i * L
+                x_new = self.prob.prox_mapping(y, 1/L_new * g_new, 1/L_new * gamma)
+                f_x = self.prob.f(x_new)
+                tmp = f_y + np.dot(g_new, x_new - y) + L_new/2 * (np.linalg.norm(y-x_new))**2 
+                oracles += 1
+                if f_x <= tmp + 1e-15 * f_y:
+                    break
+            L = L_new # L = L_new/2 (Nesterov's trick in universalFGM)
+
+            # Add adaptive restart
+            t_k_new = (1 + np.sqrt(1 + 4 * t_k**2))/2
+            y_new = x_new + (t_k - 1)/t_k_new * (x_new - x)
+            t_k = t_k_new
+            obj_new = self.prob.objective(x_new)
+            if adaptive_restart == True:
+                # Adaptive restart rule
+                if obj_new > obj:
+                    t_k = 1
+                    x = y_new
+                else:
+                    x = x_new
+            else:
+                x = x_new
+            y = y_new
+            obj = obj_new
+            values.append(obj)
+            num_oracles.append(oracles)
+
+        T_2 = time.clock()
+        print("Done: %.3f seconds"%(T_2 - T_1))
+        print("-----------------------------------")
+        
+        return np.array(values), y, np.array(num_oracles), T_2 - T_1
+    
+
     def test_all_methods_nonsmooth(self, k, beta=0.184, L_0=None, x_0=None, eps=1e-8, initial_line_search=True, draw_performance_plot=False, dataset="",  right_lim=15000):
         """
         A simple function for testing the performance of different optimizer in solving a nonsmooth problem
@@ -463,12 +547,12 @@ class First_Order_Methods:
             plt.plot(results_AC_FGM[0]-f_star, "m", linewidth=3)
             plt.plot(results_NS_PGM[0]-f_star, ":r", linewidth=3)
             plt.plot(results_NS_FGM[0]-f_star, "--k", linewidth=3)
-            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-PGM", "NS-FGM"], fontsize=15)
+            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-PGM", "NS-FGM"], fontsize=16)
             plt.yscale("log")
             plt.xticks(fontsize=15)
             plt.yticks(fontsize=15)
-            plt.ylabel("Error", fontsize=15)
-            plt.xlabel("iterations", fontsize=15)
+            plt.ylabel("Error", fontsize=18)
+            plt.xlabel("iterations", fontsize=18)
             plt.grid(linestyle= '--')
             plt.title(dataset, fontsize=22)
             plt.show()
@@ -479,19 +563,18 @@ class First_Order_Methods:
             plt.plot(results_AC_FGM[2], results_AC_FGM[0]-f_star, "m", linewidth=3)
             plt.plot(results_NS_PGM[2], results_NS_PGM[0]-f_star, ":r", linewidth=3)
             plt.plot(results_NS_FGM[2], results_NS_FGM[0]-f_star, "--k", linewidth=3)
-            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-PGM", "NS-FGM"], fontsize=15)
+            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-PGM", "NS-FGM"], fontsize=16)
             plt.yscale("log")
             plt.xticks(fontsize=15)
             plt.yticks(fontsize=15)
             plt.gca().set_xlim(right=right_lim)
-            plt.ylabel("Error", fontsize=15)
-            plt.xlabel("oracle calls", fontsize=15)
+            plt.ylabel("Error", fontsize=18)
+            plt.xlabel("oracle calls", fontsize=18)
             plt.grid(linestyle= '--')
             plt.title(dataset, fontsize=22)
             plt.show()
 
         return [results_AC_FGM, results_AC_FGM_01, results_AC_FGM_02, results_NS_PGM, results_NS_FGM], f_star
-    
 
     def test_all_methods_smooth(self, k, beta=0.184, L_0=None, x_0=None, initial_line_search=True, draw_performance_plot=False, dataset="", f_star=None, right_lim=15000, save_fig=False, save_title="plot", xsize = 15):
         """
@@ -515,19 +598,19 @@ class First_Order_Methods:
         results_AC_FGM_02 = self.universal_AC_FGM(k=k, beta = beta, x_0=x_0, alpha=0.5, eps=0, gamma=self.prob.gamma, initial_line_search=initial_line_search)
         results_adaptiveGD = self.adaptiveGD(k=k, gamma=self.prob.gamma)
         results_NS_FGM = self.UniversalFGM(k=k, gamma=self.prob.gamma, eps=0)
-        results_NS_AGD = self.nesterovAGD(k=k, gamma=self.prob.gamma, L=self.prob.L)
+        results_FISTA = self.FISTA(k=k, gamma=self.prob.gamma)
         if f_star == None:
-            f_star = min([min(results_AC_FGM[0]), min(results_AC_FGM_01[0]), min(results_AC_FGM_02[0]), min(results_adaptiveGD[0]), min(results_NS_FGM[0]), min(results_NS_AGD[0])])
+            f_star = min([min(results_AC_FGM[0]), min(results_AC_FGM_01[0]), min(results_AC_FGM_02[0]), min(results_adaptiveGD[0]), min(results_NS_FGM[0]), min(results_FISTA[0])])
 
         if draw_performance_plot == True:
             plt.figure(figsize=(9,6.5),dpi=150)
             plt.plot(results_AC_FGM_02[0]-f_star, "b", linewidth=3)
             plt.plot(results_AC_FGM_01[0]-f_star, "y", linewidth=3)
             plt.plot(results_AC_FGM[0]-f_star, "m", linewidth=3)
-            plt.plot(results_NS_AGD[0]-f_star, ":r", linewidth=3)
+            plt.plot(results_FISTA[0]-f_star, ":r", linewidth=3)
             plt.plot(results_adaptiveGD[0]-f_star, "-.g", linewidth=3)
             plt.plot(results_NS_FGM[0]-f_star, "--k", linewidth=3)
-            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-AGD", "AdGD", "NS-FGM"], fontsize=15)
+            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "FISTA", "AdGD", "NS-FGM"], fontsize=16)
             plt.yscale("log")
             plt.xticks(fontsize=xsize)
             plt.yticks(fontsize=15)
@@ -544,10 +627,10 @@ class First_Order_Methods:
             plt.plot(results_AC_FGM_02[2], results_AC_FGM_02[0]-f_star, "b", linewidth=3)
             plt.plot(results_AC_FGM_01[2], results_AC_FGM_01[0]-f_star, "y", linewidth=3)
             plt.plot(results_AC_FGM[2], results_AC_FGM[0]-f_star, "m", linewidth=3)
-            plt.plot(results_NS_AGD[2], results_NS_AGD[0]-f_star, ":r", linewidth=3)
+            plt.plot(results_FISTA[2], results_FISTA[0]-f_star, ":r", linewidth=3)
             plt.plot(results_adaptiveGD[2], results_adaptiveGD[0]-f_star, "-.g", linewidth=3)
             plt.plot(results_NS_FGM[2], results_NS_FGM[0]-f_star, "--k", linewidth=3)
-            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-AGD", "AdGD", "NS-FGM"], fontsize=15)
+            plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "FISTA", "AdGD", "NS-FGM"], fontsize=16)
             plt.yscale("log")
             plt.xticks(fontsize=xsize)
             plt.yticks(fontsize=15)
@@ -561,5 +644,149 @@ class First_Order_Methods:
             else:
                 plt.show()
 
-        return [results_AC_FGM, results_AC_FGM_01, results_AC_FGM_02, results_adaptiveGD, results_NS_FGM, results_NS_AGD], f_star
+        return [results_AC_FGM, results_AC_FGM_01, results_AC_FGM_02, results_adaptiveGD, results_NS_FGM, results_FISTA], f_star
+    
+    def test_all_methods_smooth_adaptive_restart(self, k, beta=0.184, L_0=None, x_0=None, initial_line_search=True, draw_performance_plot=False, dataset="", f_star=None, right_lim=15000, save_fig=False, save_title="plot", xsize = 15):
+        """
+        A simple function for testing the performance of different optimizer (with adaptive restart) in solving a smooth problem
+
+        Input: 
+        k: number of iterations for each method
+        beta: the beta parameter for AC-FGM
+        L_0: initial guess of the Lipschitz smooth constant. If L_0 = None, the method will use a local L_0 around x_0
+        x_0: the initialized search point
+        initial_line_search: whether performing initial line search for AC-FGM
+        draw_performance_plot: whether draw performance graph
+        dataset: name of the dateset (used in the plot name)
+        f_star: if the optimal value is known, the user can set it in advance, e.g., f_star = 0 when solving a linear system
+        right_lim: right limit for the x axis of the plots (number of iterations)
+
+        Output: output parameters of each of the optimizers, the estimated optimal function value
+        """
+        results_AC_FGM_01 = self.universal_AC_FGM(k=k, beta = beta, x_0=x_0, alpha=0.1, eps=0, gamma=self.prob.gamma, initial_line_search=initial_line_search)
+        results_FISTA = self.FISTA(k=k, gamma=self.prob.gamma)
+        results_FISTA_restart = self.FISTA(k=k, gamma=self.prob.gamma, adaptive_restart=True)
+        results_AC_FGM_restart_01 = self.universal_AC_FGM(k=k, beta = beta, x_0=x_0, alpha=0.1, eps=0, gamma=self.prob.gamma, initial_line_search=initial_line_search, adaptive_restart=True)
+
+        if f_star == None:
+            f_star = min([min(results_AC_FGM_01[0]), min(results_FISTA[0]), min(results_FISTA_restart[0]), min(results_AC_FGM_restart_01[0])])
+
+
+        if draw_performance_plot == True:
+            plt.figure(figsize=(9,6.5),dpi=150)
+            plt.plot(results_AC_FGM_01[0]-f_star, "y", linewidth=3)
+            plt.plot(results_FISTA[0]-f_star, ":r", linewidth=3)
+            plt.plot(results_AC_FGM_restart_01[0]-f_star, "--", linewidth=3)
+            plt.plot(results_FISTA_restart[0]-f_star, "-.", linewidth=3)
+            plt.legend(["AC-FGM:0.1", "FISTA", "AC-FGM:0.1 (restart)", "FISTA (restart)"], fontsize=16)
+            plt.yscale("log")
+            plt.xticks(fontsize=xsize)
+            plt.yticks(fontsize=15)
+            plt.ylabel("Error", fontsize=18)
+            plt.xlabel("Iterations", fontsize=18)
+            plt.grid(linestyle= '--')
+            plt.title(dataset, fontsize=22)
+            if save_fig == True:
+                plt.savefig("%s.png"%save_title)
+            else:
+                plt.show()
+
+            plt.figure(figsize=(9,6.5),dpi=150)
+            plt.plot(results_AC_FGM_01[2], results_AC_FGM_01[0]-f_star, "y", linewidth=3)
+            plt.plot(results_FISTA[2], results_FISTA[0]-f_star, ":r", linewidth=3)
+            plt.plot(results_AC_FGM_restart_01[2], results_AC_FGM_restart_01[0]-f_star, "--", linewidth=3)
+            plt.plot(results_FISTA_restart[2], results_FISTA_restart[0]-f_star, "-.", linewidth=3)
+            plt.legend(["AC-FGM:0.1", "FISTA", "AC-FGM:0.1 (restart)", "FISTA (restart)"], fontsize=16)
+            plt.yscale("log")
+            plt.xticks(fontsize=xsize)
+            plt.yticks(fontsize=15)
+            plt.gca().set_xlim(right=right_lim)
+            plt.ylabel("Error", fontsize=18)
+            plt.xlabel("Oracle Calls", fontsize=18)
+            plt.grid(linestyle= '--')
+            plt.title(dataset, fontsize=22)
+            if save_fig == True:
+                plt.savefig("%s_o.png"%save_title)
+            else:
+                plt.show()
+
+        return [results_AC_FGM_01, results_FISTA, results_AC_FGM_restart_01, results_FISTA_restart], f_star
+        
+
+def load_dataset_plot_nonsmooth(data, dataset="",  right_lim=15000, legend_size = 18):
+    """
+    For re-plotting with saved datasets (nonsmooth problems)
+    """
+    [results_AC_FGM, results_AC_FGM_01, results_AC_FGM_02, results_NS_PGM, results_NS_FGM], f_star = data
+    plt.figure(figsize=(9,6.5),dpi=150)
+    plt.plot(results_AC_FGM_02[0]-f_star, "b", linewidth=3)
+    plt.plot(results_AC_FGM_01[0]-f_star, "y", linewidth=3)
+    plt.plot(results_AC_FGM[0]-f_star, "m", linewidth=3)
+    plt.plot(results_NS_PGM[0]-f_star, ":r", linewidth=3)
+    plt.plot(results_NS_FGM[0]-f_star, "--k", linewidth=3)
+    plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-PGM", "NS-FGM"], fontsize=legend_size)
+    plt.yscale("log")
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.ylabel("Error", fontsize=18)
+    plt.xlabel("iterations", fontsize=18)
+    plt.grid(linestyle= '--')
+    plt.title(dataset, fontsize=22)
+    plt.show()
+
+    plt.figure(figsize=(9,6.5),dpi=150)
+    plt.plot(results_AC_FGM_02[2], results_AC_FGM_02[0]-f_star, "b", linewidth=3)
+    plt.plot(results_AC_FGM_01[2], results_AC_FGM_01[0]-f_star, "y", linewidth=3)
+    plt.plot(results_AC_FGM[2], results_AC_FGM[0]-f_star, "m", linewidth=3)
+    plt.plot(results_NS_PGM[2], results_NS_PGM[0]-f_star, ":r", linewidth=3)
+    plt.plot(results_NS_FGM[2], results_NS_FGM[0]-f_star, "--k", linewidth=3)
+    plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "NS-PGM", "NS-FGM"], fontsize=legend_size)
+    plt.yscale("log")
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.gca().set_xlim(right=right_lim)
+    plt.ylabel("Error", fontsize=18)
+    plt.xlabel("oracle calls", fontsize=18)
+    plt.grid(linestyle= '--')
+    plt.title(dataset, fontsize=22)
+    plt.show()
+
+
+def load_dataset_plot_smooth(data, dataset="", right_lim=15000, xsize = 15, legend_size = 18):
+    """
+    For re-plotting with saved datasets (smooth problems)
+    """
+    [results_AC_FGM, results_AC_FGM_01, results_AC_FGM_02, results_adaptiveGD, results_NS_FGM, results_FISTA], f_star = data
+    plt.figure(figsize=(9,6.5),dpi=150)
+    plt.plot(results_AC_FGM_02[0]-f_star, "b", linewidth=3)
+    plt.plot(results_AC_FGM_01[0]-f_star, "y", linewidth=3)
+    plt.plot(results_AC_FGM[0]-f_star, "m", linewidth=3)
+    plt.plot(results_FISTA[0]-f_star, ":r", linewidth=3)
+    plt.plot(results_adaptiveGD[0]-f_star, "-.g", linewidth=3)
+    plt.plot(results_NS_FGM[0]-f_star, "--k", linewidth=3)
+    plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "FISTA", "AdGD", "NS-FGM"], fontsize=legend_size)
+    plt.yscale("log")
+    plt.xticks(fontsize=xsize)
+    plt.yticks(fontsize=15)
+    plt.ylabel("Error", fontsize=18)
+    plt.xlabel("Iterations", fontsize=18)
+    plt.grid(linestyle= '--')
+    plt.title(dataset, fontsize=22)
+
+    plt.figure(figsize=(9,6.5),dpi=150)
+    plt.plot(results_AC_FGM_02[2], results_AC_FGM_02[0]-f_star, "b", linewidth=3)
+    plt.plot(results_AC_FGM_01[2], results_AC_FGM_01[0]-f_star, "y", linewidth=3)
+    plt.plot(results_AC_FGM[2], results_AC_FGM[0]-f_star, "m", linewidth=3)
+    plt.plot(results_FISTA[2], results_FISTA[0]-f_star, ":r", linewidth=3)
+    plt.plot(results_adaptiveGD[2], results_adaptiveGD[0]-f_star, "-.g", linewidth=3)
+    plt.plot(results_NS_FGM[2], results_NS_FGM[0]-f_star, "--k", linewidth=3)
+    plt.legend(["AC-FGM:0.5", "AC-FGM:0.1", "AC-FGM:0.0", "FISTA", "AdGD", "NS-FGM"], fontsize=legend_size)
+    plt.yscale("log")
+    plt.xticks(fontsize=xsize)
+    plt.yticks(fontsize=15)
+    plt.gca().set_xlim(right=right_lim)
+    plt.ylabel("Error", fontsize=18)
+    plt.xlabel("Oracle Calls", fontsize=18)
+    plt.grid(linestyle= '--')
+    plt.title(dataset, fontsize=22)
     
